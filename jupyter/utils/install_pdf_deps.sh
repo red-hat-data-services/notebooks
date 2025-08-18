c192296aefa2336716140b256ed7f37fe806ffb7
Jiri Daněk <jdanek@redhat.com>
2025-08-18 17:33:56 +0200

    RHAIENG-XXX: simplify PDF dependencies setup in `install_pdf_deps.sh` by replacing manual installations with `dnf` package installation
    # Conflicts:
    #	jupyter/utils/install_pdf_deps.sh

--- jupyter/utils/install_pdf_deps.sh
+++ jupyter/utils/install_pdf_deps.sh
@@ -4,37 +4,35 @@
 
 set -euxo pipefail
 
-# Mapping of `uname -m` values to equivalent GOARCH values
-declare -A UNAME_TO_GOARCH
-UNAME_TO_GOARCH["x86_64"]="amd64"
-UNAME_TO_GOARCH["aarch64"]="arm64"
-UNAME_TO_GOARCH["ppc64le"]="ppc64le"
-UNAME_TO_GOARCH["s390x"]="s390x"
+dnf install -y \
+  pandoc \
+  texlive-adjustbox \
+  texlive-bibtex \
+  texlive-charter \
+  texlive-ec \
+  texlive-euro \
+  texlive-eurosym \
+  texlive-fpl \
+  texlive-jknapltx \
+  texlive-knuth-local \
+  texlive-lm-math \
+  texlive-marvosym \
+  texlive-mathpazo \
+  texlive-mflogo-font \
+  texlive-parskip \
+  texlive-plain \
+  texlive-pxfonts \
+  texlive-rsfs \
+  texlive-tcolorbox \
+  texlive-times \
+  texlive-titling \
+  texlive-txfonts \
+  texlive-ulem \
+  texlive-upquote \
+  texlive-utopia \
+  texlive-wasy \
+  texlive-wasy-type1 \
+  texlive-wasysym \
+  texlive-xetex
 
-ARCH="${UNAME_TO_GOARCH[$(uname -m)]}"
-if [[ -z "${ARCH:-}" ]]; then
-    echo "Unsupported architecture: $(uname -m)" >&2
-    exit 1
-fi
-
-# Skip PDF export installation for s390x and ppc64le architectures
-if [[ "$(uname -m)" == "s390x" || "$(uname -m)" == "ppc64le" ]]; then
-    echo "PDF export functionality is not supported on $(uname -m) architecture. Skipping installation."
-    exit 0
-fi
-
-# tex live installation
-echo "Installing TexLive to allow PDf export from Notebooks"
-curl -fL https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -o install-tl-unx.tar.gz
-zcat < install-tl-unx.tar.gz | tar xf -
-cd install-tl-2*
-perl ./install-tl --no-interaction --scheme=scheme-small --texdir=/usr/local/texlive
-mv /usr/local/texlive/bin/"$(uname -m)-linux" /usr/local/texlive/bin/linux
-cd /usr/local/texlive/bin/linux
-./tlmgr install tcolorbox pdfcol adjustbox titling enumitem soul ucs collection-fontsrecommended
-
-# pandoc installation
-curl -fL "https://github.com/jgm/pandoc/releases/download/3.7.0.2/pandoc-3.7.0.2-linux-${ARCH}.tar.gz"  -o /tmp/pandoc.tar.gz
-mkdir -p /usr/local/pandoc
-tar xvzf /tmp/pandoc.tar.gz --strip-components 1 -C /usr/local/pandoc/
-rm -f /tmp/pandoc.tar.gz
+dnf clean all
