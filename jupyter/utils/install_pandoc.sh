a543d75775107d72fddeb15687c10e9afaeaa192
puneetsharma21 <puneet.sharma21@ibm.com>
2025-08-19 08:46:14 -0400

    install from EPEL and symlink into /usr/local/pandoc/bin

--- jupyter/utils/install_pandoc.sh
+++ jupyter/utils/install_pandoc.sh
@@ -13,49 +13,11 @@ ARCH="${UNAME_TO_GOARCH[$(uname -m)]}"
 if [[ "$ARCH" == "ppc64le" ]]; then
   # Install Pandoc from source
   dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-  dnf install -y cabal-install ghc gmp-devel gcc-toolset-13 git jq
-  
-  source /opt/rh/gcc-toolset-13/enable
-  
-  # ghc-8.10 has specific path for gcc in settings
-  sed -i.bak 's|/usr/bin/gcc|gcc|g' /usr/lib64/ghc-8.10.7/settings
-  
-  cabal user-config init
-
-  sed -i -e 's/http/https/g' -e 's/-- secure: True/secure: True/g' $HOME/.cabal/config
-  
-  cabal update
-  cabal install cabal-install --verbose
-  
-  # use latest cabal
-  export PATH=/root/.cabal/bin:$PATH
-
-  PANDOC_VERSION=$(curl -s "https://api.github.com/repos/jgm/pandoc/releases/latest" | jq -r '.tag_name')
-
-  cd /tmp
-  git clone --recurse-submodules https://github.com/jgm/pandoc.git
-  cd pandoc
-  git checkout ${PANDOC_VERSION}
-  git submodule update --init --recursive
-
-  # Modify config
-  sed -i -e 's/http/https/g' -e 's/-- secure: True/secure: True/g' $HOME/.cabal/config
-
-  cabal update -v
-  cd pandoc-cli
-  cabal build -j"$(nproc)" --verbose
+  dnf install -y pandoc
   mkdir -p /usr/local/pandoc/bin
-  cabal install \
-    --installdir=/usr/local/pandoc/bin \
-    --overwrite-policy=always \
-    --install-method=copy
-
-  # Clean up Haskell build system
-  rm -rf ~/.cabal ~/.ghc /tmp/pandoc
-  dnf remove -y cabal-install ghc gmp-devel
-  dnf clean all && rm -rf /var/cache/dnf
-
-  /usr/local/pandoc/bin/pandoc --version
+  ln -s /usr/bin/pandoc /usr/local/pandoc/bin/pandoc
+  export PATH="/usr/local/pandoc/bin:$PATH"
+  pandoc --version
 
 elif [[ "$ARCH" == "amd64" ]]; then
   # pandoc installation
