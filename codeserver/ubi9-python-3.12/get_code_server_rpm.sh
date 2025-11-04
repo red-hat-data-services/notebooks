b629cf843d15a553043d9349f824c8b23163caed
megha gaur <megha.gaur1@ibm.com>
2025-11-04 09:46:08 +0100

    added changes for node-gyp cache cleanup

--- codeserver/ubi9-python-3.12/get_code_server_rpm.sh
+++ codeserver/ubi9-python-3.12/get_code_server_rpm.sh
@@ -31,6 +31,7 @@ if [[ "$ARCH" == "amd64" || "$ARCH" == "arm64" || "$ARCH" == "ppc64le" || "$ARCH
 	# install build dependencies
 	# https://access.redhat.com/support/policy/updates/rhel-app-streams-life-cycle
 	# https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/9/html/developing_c_and_cpp_applications_in_rhel_9/assembly_additional-toolsets-for-development-rhel-9_developing-applications#cpp-compatibility-in-gcc-toolset-14_gcc-toolset-14
+	dnf groupinstall -y "Development Tools"
 	dnf install -y jq patch libtool rsync gettext gcc-toolset-14 krb5-devel libX11-devel
 
 	# starting with node-22, c++20 is required
@@ -109,6 +110,8 @@ fi
 	while IFS= read -r src_patch; do echo "patches/$src_patch"; patch -p1 < "patches/$src_patch"; done < patches/series
 	nvm use ${NODE_VERSION}
 	npm cache clean --force
+        rm -rf ~/.cache/node-gyp
+        npm install -g npm@latest	
 	npm install
 	npm run build
 	VERSION=${CODESERVER_VERSION/v/} npm run build:vscode
