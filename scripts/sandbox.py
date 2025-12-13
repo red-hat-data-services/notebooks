090d1684fcf41d63974aad83f3dfeafe178669e1
Jiri Daněk <jdanek@redhat.com>
2025-12-13 15:55:34 +0100

    NO-JIRA: chore(sandbox): add `--env=BUSTCACHE=yes01` to command args

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -48,6 +48,7 @@ def main() -> int:
     with tempfile.TemporaryDirectory(delete=True) as tmpdir:
         setup_sandbox(prereqs, pathlib.Path(tmpdir))
         command = [arg if arg != "{};" else tmpdir for arg in args.remaining[1:]]
+        command += ["--env=BUSTCACHE=yes01"]
         print(f"running {command=}")
         try:
             subprocess.check_call(command)
