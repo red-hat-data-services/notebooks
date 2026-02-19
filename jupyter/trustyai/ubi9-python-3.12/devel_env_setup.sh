792ee980dc4ca2b73cefb1242ec7e30da07ee0ab
Bhagyashri <Bhagyashri.Gaikwad2@ibm.com>
2026-02-19 17:08:10 +0530

    updated file

--- jupyter/trustyai/ubi9-python-3.12/devel_env_setup.sh
+++ jupyter/trustyai/ubi9-python-3.12/devel_env_setup.sh
@@ -75,7 +75,7 @@ if [[ $(uname -m) == "ppc64le" ]]; then
         .. && \
     make install -j ${MAX_JOBS:-$(nproc)} && \
     cd ../../python/ && \
-    uv pip install -v -r requirements-wheel-build.txt 'setuptools<82' && \
+    uv pip install -v -r requirements-wheel-build.txt && \
     PYARROW_PARALLEL=${PYARROW_PARALLEL:-$(nproc)} \
     python setup.py build_ext \
     --build-type=release --bundle-arrow-cpp \
